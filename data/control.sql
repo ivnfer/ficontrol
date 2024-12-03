ee32eb381c79f6bc0120899f3b45c9c12eff6f96
TraZno <traznodevs@gmail.com>
2024-12-03 14:01:22 +0100

    updated sql file to 2.6 changes

--- data/control.sql
+++ data/control.sql
@@ -1,4 +1,4 @@
-/* Base de datos ficontrol v2.4 */
+/* ficontrol v2.6 sql */
 
 CREATE TABLE IF NOT EXISTS screenconfig(
     id integer primary key autoincrement,
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS history_status(
 );
 
 /* INSERTS */
-/* Tabla idgroups*/
+/* idgroups*/
 INSERT INTO idgroups (id, name) VALUES (1, 'inputvalue');
 INSERT INTO idgroups (id, name) VALUES (2, 'energymodevalue');
 INSERT INTO idgroups (id, name) VALUES (3, 'onewirevalue');
@@ -56,7 +56,7 @@ INSERT INTO idgroups (id, name) VALUES (4, 'volumevalue');
 INSERT INTO idgroups (id, name) VALUES (5, 'mutevalue');
 INSERT INTO idgroups (id, name) VALUES (6, 'powervalue');
 
-/* Tabla hexcodes */
+/* hexcodes */
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (1, 'HDMI 1', '0d');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (1, 'HDMI 2', '06');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (1, 'HDMI 3', '0f');
@@ -65,10 +65,10 @@ INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (1, 'Browser', '10');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (1, 'Display Port 1', '0a');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (1, 'CMND&Play', '11');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (1, 'NACK', '15');
-INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (2, 'MODO 1', '04');
-INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (2, 'MODO 2', '05');
-INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (2, 'MODO 3', '06');
-INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (2, 'MODO 4', '07');
+INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (2, '1', '04');
+INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (2, '2', '05');
+INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (2, '3', '06');
+INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (2, '4', '07');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (2, 'NACK', '15');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (3, 'OFF', '00');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (3, 'ON', '01');
@@ -80,6 +80,6 @@ INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (6, 'OFF', '01');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (6, 'ON', '02');
 INSERT INTO hexcodes (idtype, codename, hexcode) VALUES (6, 'NACK', '15');
 
-/* Tabla screenconfig */
+/* screenconfig */
 INSERT INTO screenconfig (model, platform, powersavingmode, onewire) VALUES ('32BDL3550Q', 'BDL3550Q', '0x06', '0x00');
 INSERT INTO screenconfig (model, platform, powersavingmode, onewire) VALUES ('43BDL4550D', 'BDL4550D', '0x06', '0x00');
